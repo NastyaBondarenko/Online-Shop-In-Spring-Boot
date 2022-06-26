17e2e8b67ee2f323a34af8a231a749579d86e3b1
NastyaBondarenko <av_bondarenko@rambler.ru>
2022-06-26 19:12:35 +0300

    refactored ProductControllerTest:
    - add tests.

--- src/test/java/com/bondarenko/onlineshop/web/controller/ProductControllerTest.java
+++ src/test/java/com/bondarenko/onlineshop/web/controller/ProductControllerTest.java
@@ -17,8 +17,6 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import java.time.LocalDateTime;
 import java.util.Optional;
 
-import static org.springframework.test.util.AssertionErrors.assertNotNull;
-import static org.springframework.test.util.AssertionErrors.assertNull;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -45,8 +43,8 @@ class ProductControllerTest {
     }
 
     @Test
-    @DisplayName("add Product")
-    void addProduct() throws Exception {
+    @DisplayName("when Add Product then Request Has Succeeded and Product Added")
+    void whenAddProduct_thenRequestHasSucceeded_andProductAdded() throws Exception {
         Product inputProduct = Product.builder()
                 .id(1)
                 .name("TV")
@@ -67,16 +65,8 @@ class ProductControllerTest {
     }
 
     @Test
-    @DisplayName("update Product")
-    void updateProduct() throws Exception {
-        Product inputProduct = Product.builder()
-                .id(1)
-                .name("TV")
-                .price(3000)
-                .creationDate(LocalDateTime.now())
-                .build();
-
-//        productService.update(1, inputProduct);
+    @DisplayName("when Update Product then Request Has Succeeded and Product Updated")
+    void whenUpdateProduct_thenRequestHasSucceeded_andProductUpdated() throws Exception {
 
         mockMvc.perform(MockMvcRequestBuilders.put("/products/update/1")
                         .contentType(MediaType.APPLICATION_JSON)
@@ -89,9 +79,8 @@ class ProductControllerTest {
     }
 
     @Test
-    @DisplayName("find Product By Id ")
-    void findById() throws Exception {
-//        productService.findById(1);
+    @DisplayName("when Find Product By Id then Request Has Succeeded")
+    void whenFindProductById_thenRequestHasSucceeded() throws Exception {
         mockMvc.perform(MockMvcRequestBuilders.get("/products/1")
                         .contentType(MediaType.APPLICATION_JSON)
                 )
@@ -99,18 +88,16 @@ class ProductControllerTest {
     }
 
     @Test
-    @DisplayName("search Product")
-    void searchProduct() throws Exception {
-//        productService.findProductByName("TV");
-
+    @DisplayName("when Search Product then Request Has Succeeded")
+    void whenSearchProduct_thenRequestHasSucceeded() throws Exception {
         mockMvc.perform(MockMvcRequestBuilders.get("/products/search/TV")
                         .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(MockMvcResultMatchers.status().isOk());
     }
 
     @Test
-    @DisplayName("findAll Product")
-    void findAll() throws Exception {
+    @DisplayName("when Find All then Request Has Succeeded")
+    void whenFindAll_thenRequestHasSucceeded() throws Exception {
 
         mockMvc.perform(MockMvcRequestBuilders.get("/products")
                         .contentType(MediaType.APPLICATION_JSON))
@@ -119,13 +106,14 @@ class ProductControllerTest {
 
 
     @Test
-    @DisplayName("delete Product")
-    void deleteProduct() throws Exception {
-//        productService.delete(1);
+    @DisplayName("when Delete Product then Request Has Succeeded")
+    void whenDeleteProduct_thenRequestHasSucceeded() throws Exception {
+
         mockMvc.perform(MockMvcRequestBuilders.delete("/products/delete/1")
                         .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(MockMvcResultMatchers.status().isOk());
     }
+
 
     @Test
     @DisplayName("when Find Product By Id then Searched Product Return ")
@@ -141,5 +129,88 @@ class ProductControllerTest {
                         .value(product.getPrice()));
     }
 
+    @Test
+    @DisplayName("when Add Product with Incorrect Url then Method Not Allowed Return")
+    void whenAddProduct_withIncorrectUrl_thenMethodNotAllowedReturn() throws Exception {
+        Product inputProduct = Product.builder()
+                .id(1)
+                .name("TV")
+                .price(3000)
+                .creationDate(LocalDateTime.now())
+                .build();
+
+        productService.add(inputProduct);
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/products/IncorrectUrl")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("""
+                                {
+                                "name":"TV",
+                                "price":"3000"
+                                }"""))
+                .andExpect(MockMvcResultMatchers.status().isMethodNotAllowed());
+    }
+
+    @Test
+    @DisplayName("when Update Product with Incorrect Url then Method Not Allowed Return")
+    void whenUpdateProduct_withIncorrectUrl_thenMethodNotAllowedReturn() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.put("/products/IncorrectUrl")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("""
+                                {
+                                "name":"Snowboard",
+                                "price":"2000"
+                                }"""))
+                .andExpect(MockMvcResultMatchers.status().isMethodNotAllowed());
+    }
+
+    @Test
+    @DisplayName("when Find Product By Id with Incorrect Id then Bad Request Return")
+    void whenFindProductById_withIncorrectId_thenBadRequestReturn() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/products/IncorrectId")
+                        .contentType(MediaType.APPLICATION_JSON)
+                )
+                .andExpect(MockMvcResultMatchers.status().isBadRequest());
+    }
+
+    @Test
+    @DisplayName("when Update Product with Incorrect Url then Method Not Found Return")
+    void whenUpdateProduct_withIncorrectUrl_thenMethodNotFoundReturn() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/products/IncorrectUrl/TV")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isNotFound());
+    }
+
+    @Test
+    @DisplayName("when Find All with Incorrect Url then Not Found Return")
+    void whenFindAll_withIncorrectUrl_thenNotFoundReturn() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/IncorrectUrl")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isNotFound());
+    }
+
+    @Test
+    @DisplayName("when Delete Product with Incorrect Url then Not Found Return")
+    void whenDeleteProduct_withIncorrectUrl_thenNotFoundReturn() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.delete("/products/IncorrectUrl/1")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isNotFound());
+    }
+
+    @Test
+    @DisplayName("when Delete Product with Incorrect Id then Bad Request Return")
+    void whenDeleteProduct_withIncorrectId_thenBadRequestReturn() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.delete("/products/delete/incorrectId")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isBadRequest());
+    }
+
+    @Test
+    @DisplayName("when Search Product with Incorrect Url then Bad Request Return")
+    void whenSearchProduct_withIncorrectUrl_thenBadRequestReturn() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/products/search//")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isBadRequest());
+    }
 
 }
