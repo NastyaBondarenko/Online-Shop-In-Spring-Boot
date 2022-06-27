05343adce9f488c4df376f86734efe725dbc90c6
NastyaBondarenko <av_bondarenko@rambler.ru>
2022-06-27 19:41:29 +0300

    refactored ProductRepositoryTest:
    - renamed tests.

--- src/test/java/com/bondarenko/onlineshop/repositary/ProductRepositoryTest.java
+++ src/test/java/com/bondarenko/onlineshop/repositary/ProductRepositoryTest.java
@@ -1,15 +1,21 @@
 package com.bondarenko.onlineshop.repositary;
 
 import com.bondarenko.onlineshop.entity.Product;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.dao.InvalidDataAccessApiUsageException;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
 @DataJpaTest
 class ProductRepositoryTest {
 
@@ -21,7 +27,7 @@ class ProductRepositoryTest {
 
     @BeforeEach
     void setUp() {
-      Product  product = Product.builder()
+        Product product = Product.builder()
                 .name("TV")
                 .price(3000)
                 .creationDate(LocalDateTime.now())
@@ -31,10 +37,20 @@ class ProductRepositoryTest {
     }
 
     @Test
-    public void whenFindById_thenReturn() {
-        Product product = productRepository.findById(1).get();
-        assertEquals(product.getName(), "TV");
-        assertEquals(product.getPrice(), 3000);
+    @DisplayName("when Find Product By Name Ignore Case then Correct Data Return")
+    public void whenFindProductByNameIgnoreCase_thenCorrectDataReturn() {
+        List<Product> list = productRepository.findProductByNameIgnoreCase("tv");
+        assertEquals("TV", list.get(0).getName());
+        assertEquals(3000, list.get(0).getPrice());
     }
 
+    @Test
+    @DisplayName("when Find By Id With Incorrect Id then InvalidDataAccessApiUsageException Return")
+    public void whenFindByIdWithIncorrectId_thenInvalidDataAccessApiUsageExceptionReturn() {
+        Assertions.assertThrows(InvalidDataAccessApiUsageException.class, () -> {
+            Product product = productRepository.findById(null).get();
+            assertNull(product.getName());
+            assertNull(product.getPrice());
+        });
+    }
 }
