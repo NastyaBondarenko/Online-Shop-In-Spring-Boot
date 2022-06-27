ae8eeaf16f43f0d7f14615ed30e0ba0baceba594
NastyaBondarenko <av_bondarenko@rambler.ru>
2022-06-27 19:30:36 +0300

    add ProductNotFoundExceptions

---
+++ src/main/java/com/bondarenko/onlineshop/exceptions/ProductNotFoundExceptions.java
@@ -0,0 +1,8 @@
+package com.bondarenko.onlineshop.exceptions;
+
+public class ProductNotFoundExceptions extends Exception {
+
+    public ProductNotFoundExceptions(String message) {
+        super(message);
+    }
+}
